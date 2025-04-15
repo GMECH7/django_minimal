33aef5eb983db38354719ef584f772cfada67e04
Giorgos <gm7.gavroleon@gmail.com>
2025-04-15 14:53:49 +0300

    Created venv and requirements.txt

--- base_app/views.py
+++ base_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Create your views here.def home():
+def home(request):
+    return HttpResponse("Hello, world! This is an attempt to host my app on Linux.")
